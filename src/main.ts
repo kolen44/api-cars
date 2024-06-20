91c6caf3ccf7f8f8d6f25783276913cb2b1ce1de
kolen44 <malavov70@gmail.com>
2024-06-20 20:17:20 +0300

    feat: enhance error handling and logging for application bootstrap - Added try-catch block to capture and log errors during application startup - Improved logging to provide more detailed information about each step of the bootstrap process - Configured application to use environment variable for port selection with a default fallback to 3000

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,27 @@ import * as bodyParser from 'body-parser';
 import { config } from 'dotenv';
 import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
-  config();
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  app.use(json({ limit: '50mb' }));
-  app.use(urlencoded({ limit: '50mb', extended: true }));
-  await app.listen(3000);
-  console.log(`Listening on url ${await app.getUrl()}`);
+  try {
+    console.log('Loading environment variables...');
+    config();
+    console.log('Creating NestJS application...');
+    const app = await NestFactory.create(AppModule);
+    console.log('Configuring application...');
+    app.enableCors();
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
+    app.use(bodyParser.json({ limit: '50mb' }));
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ limit: '50mb', extended: true }));
+
+    const PORT = process.env.PORT || 3000;
+    console.log(`Starting application on port ${PORT}...`);
+    await app.listen(PORT);
+    console.log(`Listening on url ${await app.getUrl()}`);
+  } catch (error) {
+    console.error('Error during application bootstrap:', error);
+  }
 }
+
 bootstrap();
